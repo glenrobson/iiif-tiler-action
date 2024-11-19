e73562f495a689055b1a2d396d5359f1b8ed6cc5
Glen Robson <glen.robson@gmail.com>
2024-11-19 22:55:47 +0000

    Sorting path

--- iiif_tiler_action/convertImages.py
+++ iiif_tiler_action/convertImages.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 import os, sys
-sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from . import updateManifest
 
 def getUserRepo():
